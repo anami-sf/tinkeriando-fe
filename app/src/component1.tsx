a0038c10cb4ac4b6e23cff453819155134c9b115
anami-sf <anami.127.0.0.1@gmail.com>
2024-03-16 16:45:30 -0700

    Dockerize application

---
+++ app/src/component1.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react';
+
+const FetchGetRequest = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchDataForPosts = async () => {
+      try {
+        const response = await fetch(
+          `http://0.0.0.0:8081`
+        );
+        if (!response.ok) {
+          throw new Error(`HTTP error: Status ${response.status}`);
+        }
+        let postData = await response.json();
+        setData(postData.Hello);
+        setError(null);
+      } catch (err) {
+        setError(err.message);
+        setData(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchDataForPosts();
+  }, []);
+
+  return (
+    <div>
+      {data}
+    </div>
+  );
+};
+
+export default FetchGetRequest;
